9fd979cfa6b5cd9ab1703ca3ef47a1e51b466899
Tyler Collingridge <tyler@MacBook-Pro.local>
2025-01-01 13:00:42 -0700

    feat: create array in SortPage
    - Style array bars (note: text alignment is a little weird right now,
    it is not centered on the bar, however, you can see the text.)
    - Create resetArray to generate new array on mount and when the reset
    button is clicked.

--- src/components/SortPage/SortPage.jsx
+++ src/components/SortPage/SortPage.jsx
@@ -1,18 +1,63 @@
+import { useCallback, useEffect, useState } from "react";
 import PageLayout from "../AlgoPageLayout/PageLayout.jsx";
+import classes from "./SortPage.module.css";
+import {
+  ARRAY_SIZE,
+  MAX_VAL,
+  BAR_HEIGHT,
+  DEFAULT_BAR,
+  MIN_VAL,
+} from "./constants.js";
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+const randomIntFromInterval = (min, max) => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 // This page will create a binary tree using D3.js and then the user can choose to use BFS or DFS to find a target element
 const SortPage = () => {
+  const [array, setArray] = useState([]);
+
+  const resetArray = useCallback(() => {
+    const newArray = [];
+    for (let i = 0; i < ARRAY_SIZE; i++) {
+      const randomVal = randomIntFromInterval(MIN_VAL, MAX_VAL);
+      newArray.push(randomVal);
+    }
+    console.log(newArray);
+    setArray(newArray);
+  }, []);
+
+  useEffect(() => {
+    resetArray();
+  }, [resetArray]);
+
   const leftContent = (
     <>
       <h1>Sorting Algorithms</h1>
       <p>Description</p>
+      <button onClick={resetArray}>Reset</button>
     </>
   );
 
   const rightContent = (
-    <>
-      <div>Right Content Test</div>
-    </>
+    <div className={classes.arrayContainer}>
+      <div>
+        {array.map((value, index) => (
+          <div
+            className={classes.arrayBar}
+            key={index}
+            style={{
+              height: `${(value / MAX_VAL) * 50 + 3}vh`, // Set height proportionate to MAX_VAL
+              // width: `${Math.min(100 / ARRAY_SIZE, 20)}px`, // Dynamically adjust bar width based on array size
+            }}
+          >
+            <div className={classes.barText}>{value}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 
   return <PageLayout leftContent={leftContent} rightContent={rightContent} />;
---
+++ src/components/SortPage/constants.js
@@ -0,0 +1,10 @@
+export const ARRAY_SIZE = 40;
+export const MIN_VAL = 5; // any int less is not visible
+export const MAX_VAL = 500;
+
+// array bar colors
+export const DEFAULT_BAR = "#007bff";
+export const COMPARED_BAR = "red"; // when two elements are compared during sorting
+export const SORTED_BAR = "green";
+
+export const BAR_HEIGHT = "100";
